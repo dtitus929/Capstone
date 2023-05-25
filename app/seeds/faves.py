900fec275e2ed477e43ca018ed39124169679442
dtitus929 <dave@creativegozone.com>
2023-05-25 16:46:47 -0400

    Increased Fave Seed Data for 3 Users

--- app/seeds/faves.py
+++ app/seeds/faves.py
@@ -7,10 +7,22 @@ def seed_faves():
     fave1 = Fave(name='Art', url='https://www.art.com/', user_id=1)
     fave2 = Fave(name='Google', url='https://www.google.com/', user_id=1)
     fave3 = Fave(name='DB Schema', url='https://dbdiagram.io/d/643dba9f6b31947051bfe073',  user_id=1)
+    fave4 = Fave(name='Art u2', url='https://www.art.com/', user_id=2)
+    fave5 = Fave(name='Google u2', url='https://www.google.com/', user_id=2)
+    fave6 = Fave(name='DB Schema u2', url='https://dbdiagram.io/d/643dba9f6b31947051bfe073',  user_id=2)
+    fave7 = Fave(name='Art u3', url='https://www.art.com/', user_id=3)
+    fave8 = Fave(name='Google u3', url='https://www.google.com/', user_id=3)
+    fave9 = Fave(name='DB Schema u3', url='https://dbdiagram.io/d/643dba9f6b31947051bfe073',  user_id=3)
 
     db.session.add(fave1)
     db.session.add(fave2)
     db.session.add(fave3)
+    db.session.add(fave4)
+    db.session.add(fave5)
+    db.session.add(fave6)
+    db.session.add(fave7)
+    db.session.add(fave8)
+    db.session.add(fave9)
 
     db.session.commit()
 
